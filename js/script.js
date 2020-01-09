0648d6baa6750f0a4b3a07b8c11adab6091add70
harshay <harshaykr@hotmail.com>
2020-01-09 22:15:41 +0000

    functions reworking 1

--- js/script.js
+++ js/script.js
@@ -11,24 +11,14 @@ Contact : rharshay@gmail.com
 
 //Get all student list elements by class name 
 const studList  = document.getElementsByClassName("student-item cf"); 
-//Get all tags containing student names (used for search functionality)
-const studNameList  = document.getElementsByTagName("h3");
-//number of students on the page 
-const studNum   = studList.length; 
-//total number of pages to split the list by
-const pagesNum  = Math.ceil((studNum/10)); 
 //get class that contains the student list html
 const listDiv   = document.getElementsByClassName("student-list")[0]; 
-//capture div element ; used for getting ul tag's parent element
-const studUl = document.getElementsByTagName("ul")[0];
-//get parent element to which navigation/pagination buttons will be appended
-const studDiv = studUl.parentNode; 
 
 
 /**********************************************************************************************************/
 // create function which will display a maximum of 10 stuent list items from the page selected
 
-const showPage = (selectedPage) => {
+const showPage = (selectedPage,listIn) => {
 
    //display a maximum of 10 students on the page when the page loads
       const firstPage = selectedPage; 
@@ -40,7 +30,7 @@ const showPage = (selectedPage) => {
    //limits above which change depending on the page selected
       for(let i = 0; i < studNum; i += 1) {
    
-         let studListElem = studList[i];
+         let studListElem = listIn[i];
    
          if(i >= lowerLimit  && i <= upperLimit)  {    
             studListElem.style.display = ''; 
@@ -52,35 +42,51 @@ const showPage = (selectedPage) => {
    }
 /**********************************************************************************************************/
    //on page load display first 10 items from the list
-   showPage(1);
+   showPage(1,studList);
 
 /**********************************************************************************************************/
 //create pagination/links
 
+
+const pagination = (listIn) => {   
+
+//capture div element ; used for getting ul tag's parent element
+const studUl = document.getElementsByTagName("ul")[0];
+//get parent element to which navigation/pagination buttons will be appended
+const studDiv = studUl.parentNode; 
+//number of students on the page 
+const studNum   = listIn.length; 
+//total number of pages to split the list by
+const pagesNum  = Math.ceil((studNum/10)); 
+////////////////////////////////////////////////////////
 const navDiv = document.createElement("div"); 
 studDiv.appendChild(navDiv); 
 navDiv.className = "pagination"; 
 
 //loop will create all the buttons required for the navigation & apply click event listener to each
-for(let i = 0; i < pagesNum; i += 1) {
+   for(let i = 0; i < pagesNum; i += 1) {
 
+      //create list item
+      let navListItem = document.createElement('li'); 
+      //append list item to navigation div
+      navDiv.appendChild(navListItem);
+      //create a tag which will trigger the showPage function depending on the selecte page
+      let navListLink = document.createElement('a');
+      
+      navListLink.textContent =  (i + 1);   
+      
+      if(i === 0)  {
+         navListLink.className = "active"; 
+      }
 
-   //create list item
-   let navListItem = document.createElement('li'); 
-   //append list item to navigation div
-   navDiv.appendChild(navListItem);
-   //create a tag which will trigger the showPage function depending on the selecte page
-   let navListLink = document.createElement('a');
-     
-   navListLink.textContent =  (i + 1);   
-   
-   if(i === 0)  {
-      navListLink.className = "active"; 
-   }
+      navListItem.appendChild(navListLink);     
 
-   navListItem.appendChild(navListLink);     
-
+   };
 };
+
+/**********************************************************************************************************/
+//on page load create pagination elements
+pagination(studList);
 
 
 /**********************************************************************************************************/
@@ -137,33 +143,22 @@ searchDiv.appendChild(searchButton);
 
 // element index numbers which match the text which was searched will be pushed to this array
 let searchMatch = []; 
-let searchHtml  = ''; 
 
 searchButton.addEventListener('click', (event) => {  
    
 // clear array for a new search
 searchMatch = []; 
-// clear html that contains the matched search elements that will be inserted
-searchHtml = ''; 
 
 // pick up all student list elements that match and push their index number to an array
    for(let k = 0; k < studNameList.length; k += 1) {   
 
       if(studNameList[k].textContent.toUpperCase().includes(searchBox.value.toUpperCase())) {;
-          searchMatch.push(k);
+          searchMatch.push(studList[k]); //how do I view contents this element in the console?
          };
    };
 
-// construct html using student list items which match the search text
-   for(let l = 0; l < searchMatch.length; l += 1) {   
+   console.log(searchMatch);
 
-      searchHtml = searchHtml + studList[searchMatch[l]];
-
-   };
-
-   studUl.innerHTML = searchHtml; 
-
-   //console.log(searchHtml);
 
 });
 
